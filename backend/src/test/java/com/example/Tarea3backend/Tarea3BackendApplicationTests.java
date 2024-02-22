9851e3f277ae71a966f449a1bd7031668bc98b07
dam2 <dam2@Mac-Mini-07.local>
2024-02-22 16:48:22 +0100

    commit 1

---
+++ backend/src/test/java/com/example/Tarea3backend/Tarea3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarea3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
